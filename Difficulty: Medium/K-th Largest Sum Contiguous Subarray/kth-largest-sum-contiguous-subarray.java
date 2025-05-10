61d09e1bc8afc492b80d1ecb948ced761ac2ea27
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-05-11 02:57:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public static int kthLargest(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        // Min-heap to store k largest sums
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        // Generate all subarray sums
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (minHeap.size() < k) {
+                    minHeap.offer(sum);
+                } else if (sum > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.offer(sum);
+                }
+            }
+        }
+
+        // The top of the heap is the k-th largest sum
+        return minHeap.peek();
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+            int k = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.kthLargest(arr, k);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
