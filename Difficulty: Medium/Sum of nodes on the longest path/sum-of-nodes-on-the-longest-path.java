c4ec85b14745d03968327152c7661b11fde1840b
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-05-29 08:50:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of nodes on the longest path/sum-of-nodes-on-the-longest-path.java
@@ -0,0 +1,45 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data){
+        this.data = data;
+    }
+} */
+class Solution {
+    // Helper class to store max length and max sum
+    static class Result {
+        int maxLen = 0;
+        int maxSum = 0;
+    }
+
+    public int sumOfLongRootToLeafPath(Node root) {
+        Result res = new Result();
+        dfs(root, 0, 0, res);
+        return res.maxSum;
+    }
+
+    // DFS helper method
+    private void dfs(Node node, int currLen, int currSum, Result res) {
+        if (node == null) return;
+
+        currLen += 1;
+        currSum += node.data;
+
+        // If it's a leaf node
+        if (node.left == null && node.right == null) {
+            if (currLen > res.maxLen) {
+                res.maxLen = currLen;
+                res.maxSum = currSum;
+            } else if (currLen == res.maxLen) {
+                res.maxSum = Math.max(res.maxSum, currSum);
+            }
+            return;
+        }
+
+        // Recur on left and right
+        dfs(node.left, currLen, currSum, res);
+        dfs(node.right, currLen, currSum, res);
+    }
+}
